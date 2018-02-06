608f33ff89a3b837e8015bf93f5e1a1ee2a53e7e
fzy <13581473784@163.com>
2018-02-06 17:25:24 +0800

    loading组件

--- src/base/loading/loading.vue
+++ src/base/loading/loading.vue
@@ -1,28 +1,76 @@
 <template>
-  <div>
-    <div class="loading"></div>
+  <div class="loading">
+    <div class="box">
+      <svg class="load_ellipse" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <ellipse cx="26" cy="10" rx="26" ry="10" style="fill:#ddd;stroke:none;"></ellipse>
+      </svg>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style lang="less" scoped>
 @import "../../style/mixin.less";
 .loading {
-  .center();
-  .wh(100px, 100px);
-  border: 1px solid #333;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 13;
+  background-color: #fff;
+}
+
+.box {
+  width: 100px;
+  height: 100px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
   background: url("../../assets/img/icon_loading.png") no-repeat;
-  animation: loading steps(6) 7s infinite;
-  @keyframes loading {
-    from {
-      background-position-y: 0px;
-    }
-    to {
-      background-position-y: -600px;
-    }
+  animation: move 0.7s infinite ease-in-out, loading steps(6) 7s infinite;
+}
+.load_ellipse {
+  width: 54px;
+  height: 20px;
+  position: absolute;
+  left: 50%;
+  bottom: -30px;
+  transform: translate(-50%, 0);
+  animation: scale 0.7s infinite ease-in-out;
+}
+@keyframes scale {
+  0% {
+    transform: translate(-50%, 0) scale(1);
+  }
+  50% {
+    transform: translate(-50%, 0) scale(0.6);
+  }
+  100% {
+    transform: translate(-50%, 0) scale(1);
+  }
+}
+@keyframes move {
+  0% {
+    transform: translate(-50%, 0px);
+  }
+  50% {
+    transform: translate(-50%, -50px);
+  }
+  100% {
+    transform: translate(-50%, 0px);
+  }
+}
+@keyframes loading {
+  from {
+    background-position-y: 0px;
+  }
+  to {
+    background-position-y: -600px;
   }
 }
 </style>
--- src/components/coupon/children/commend.vue
+++ src/components/coupon/children/commend.vue
@@ -1,23 +1,36 @@
 <template>
   <div>
     <my-header :title="title" :isSearch="isSearch" :isBack="isBack" :isLogin="isLogin"></my-header>
-    
+    <loading v-show="loading"></loading>
   </div>
 </template>
 
 <script>
 import myHeader from "@/components/header/header";
+import Loading from "@/base/loading/loading";
 export default {
   data() {
     return {
       isBack: true,
       isSearch: false,
       isLogin: false,
-      title: "推荐有奖"
+      title: "推荐有奖",
+      loading: true
     };
   },
   components: {
-    myHeader
+    myHeader,
+    Loading
+  },
+  mounted() {
+    this.closeLoading();
+  },
+  methods: {
+    closeLoading() {
+      setTimeout(() => {
+        this.loading = false;
+      }, 2000);
+    }
   }
 };
 </script>
